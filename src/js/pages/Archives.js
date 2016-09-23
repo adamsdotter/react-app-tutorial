c83b7fcd929733a7ca5b474d469b03e66cae076b
padamsson <patricia.adamsson@creuna.se>
2016-09-23 10:39:17 +0200

    Print archives article

--- src/js/pages/Archives.js
+++ src/js/pages/Archives.js
@@ -2,10 +2,13 @@ import React from 'react';
 
 export default class Archives extends React.Component {
   render() {
+    const { article } = this.props.params;
+    const articleName = article ? '(' + article + ')' : '';
+
     return (
       <div class="col-md-4">
-        <h2>Archives</h2>
-        <p>Es ist ein lang erwiesener Fakt, dass ein Leser vom Text abgelenkt wird, wenn er sich ein Layout ansieht. Der Punkt, Lorem Ipsum zu nutzen, ist, dass es mehr oder weniger die normale Anordnung von Buchstaben darstellt und somit nach lesbarer Sprache aussieht. Viele Desktop Publisher und Webeditoren nutzen mittlerweile Lorem Ipsum als den Standardtext, auch die Suche im Internet nach "lorem ipsum" macht viele Webseiten sichtbar, wo diese noch immer vorkommen.</p>
+        <h2>Archives {articleName}</h2>
+        <p>Hello!</p>
         <a class="btn btn-default" href="#">More Info</a>
     </div>
     );
