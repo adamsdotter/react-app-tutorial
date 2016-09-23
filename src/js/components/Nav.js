b18c65d86f797c32a58bd57033d5ffdb04fb6c81
padamsson <patricia.adamsson@creuna.se>
2016-09-23 10:56:14 +0200

    Add active classes and collapse nav

--- src/js/components/Nav.js
+++ src/js/components/Nav.js
@@ -2,28 +2,47 @@ import React from 'react';
 import { Link, IndexLink } from 'react-router';
 
 export default class Nav extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      collapsed: true
+    };
+  }
+
+  toggleCollapse() {
+    const collapsed = !this.state.collapsed;
+    this.setState({collapsed});
+  }
+
   render() {
+    const { collapsed } = this.state;
+    const { location } = this.props;
+    const featureClass = location.pathname === '/' ? 'active' : '';
+    const settingsClass = location.pathname.match(/^\/settings/) ? 'active' : '';
+    const archivesClass = location.pathname.match(/^\/archvies/) ? 'active' : '';
+    const navClass = collapsed ? 'collapse' : '';
+
     return (
       <nav class="navbar navbar-inverse navbar-fixed-top" role="navigation">
         <div class="container">
           <div class="navbar-header">
-            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1">
+            <button type="button" class="navbar-toggle" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" onClick={this.toggleCollapse.bind(this)}>
               <span class="sr-only">Toggle navigation</span>
               <span class="icon-bar"></span>
               <span class="icon-bar"></span>
               <span class="icon-bar"></span>
             </button>
           </div>
-          <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+          <div class={"navbar-collapse " + navClass} id="bs-example-navbar-collapse-1">
             <ul class="nav navbar-nav">
-              <li>
-                <IndexLink to="/" activeClassName="active">Featured</IndexLink>
+              <li class={featureClass}>
+                <IndexLink to="/">Featured</IndexLink>
               </li>
-              <li>
-                <Link to="archives" activeClassName="active">Archives</Link>
+              <li class={archivesClass}>
+                <Link to="archives">Archives</Link>
               </li>
-              <li>
-                <Link to="settings" activeClassName="active">Settings</Link>
+              <li class={settingsClass}>
+                <Link to="settings">Settings</Link>
               </li>
             </ul>
           </div>
--- src/js/pages/Layout.js
+++ src/js/pages/Layout.js
@@ -5,12 +5,13 @@ import Nav from '../components/Nav';
 
 export default class Layout extends React.Component {
   render() {
+    const { location } = this.props;
     const containerStyle = {
       marginTop: '60px'
     };
     return (
       <div>
-        <Nav />
+        <Nav location={location} />
         <div class="container" style={containerStyle}>
           <div class="row">
             <div class="col-lg-12">
