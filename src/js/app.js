75b015ff8f9a7dd16c0efb12e685e41f31c945d9
padamsson <patricia.adamsson@creuna.se>
2016-09-23 10:39:45 +0200

    Add optional params to archive route

--- src/js/app.js
+++ src/js/app.js
@@ -8,11 +8,12 @@ import Layout from './pages/Layout';
 import Settings from './pages/Settings';
 
 var app = document.getElementById('app');
+
 ReactDOM.render(
   <Router history={hashHistory}>
     <Route path="/" component={Layout}>
       <IndexRoute component={Featured}></IndexRoute>
-      <Route path="archives" component={Archives}></Route>
+      <Route path="archives(/:article)" component={Archives}></Route>
       <Route path="settings" component={Settings}></Route>
     </Route>
   </Router>,
