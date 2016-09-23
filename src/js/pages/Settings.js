263442a40b7286e5610abe5d76ba58e7580e11d8
padamsson <patricia.adamsson@creuna.se>
2016-09-23 10:40:10 +0200

    Remove annoying german text

--- src/js/pages/Settings.js
+++ src/js/pages/Settings.js
@@ -5,7 +5,7 @@ export default class Settings extends React.Component {
     return (
       <div class="col-md-4">
         <h2>Settings</h2>
-        <p>Es gibt viele Variationen der Passages des Lorem Ipsum, aber der Hauptteil erlitt Änderungen in irgendeiner Form, durch Humor oder zufällige Wörter welche nicht einmal ansatzweise glaubwürdig aussehen. Wenn du eine Passage des Lorem Ipsum nutzt, solltest du aufpassen dass in der Mitte des Textes keine ungewollten Wörter stehen. Viele der Generatoren im Internet neigen dazu, vorgefertigte Stücke zu wiederholen - was es nötig machte einen richtigen Generator zu entwickeln.</p>
+        <p>Eniam quaerat eveniet eligendi dolor</p>
         <a class="btn btn-default" href="#">More Info</a>
     </div>
     );
